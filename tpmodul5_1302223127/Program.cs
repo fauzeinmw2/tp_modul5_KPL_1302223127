9df945186643acf22de2c8d2a8d2b3b2ac27d083
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2024-03-17 22:08:17 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223127/Program.cs
+++ tpmodul5_1302223127/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    class DataGeneric<T>
+    {
+        private T data;
 
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang Tersimpan Adalah: {data}");
+        }
+
+    }
+
+    private static void Main(string[] args)
+    {
+        DataGeneric<string> objStr = new DataGeneric<string>("1302223127");
+        objStr.PrintData();
+
+        DataGeneric<int> objInt = new DataGeneric<int>(1302223127);
+        objInt.PrintData();
+    }
+}
